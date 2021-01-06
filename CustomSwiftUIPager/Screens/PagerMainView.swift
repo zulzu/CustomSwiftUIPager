edf5fbbf77bcc7e13db0708afe6f59a325e7dcba
Andras Pal <andr4s.pal@gmail.com>
2021-01-06 18:00:10 +0000

    Add circleSize and pageCount, add tappable page indicator dots to the bottom of the screen

--- CustomSwiftUIPager/Screens/PagerMainView.swift
+++ CustomSwiftUIPager/Screens/PagerMainView.swift
@@ -11,19 +11,40 @@ struct PagerMainView: View {
     // # Private/Fileprivate
     // The current page of the PagerView
     @State private var currentPage = 0
+    // The size of the page indicator dots
+    private let circleSize: CGFloat = 8
+    // The number of the pages
+    private let pageCount: Int = 3
     
     // # Body
     var body: some View {
         
         ZStack {
             
-            PagerView(pageCount: 3, currentIndex: self.$currentPage) {
+            PagerView(pageCount: pageCount, currentIndex: self.$currentPage) {
                 PagerView1()
                 PagerView2()
                 PagerView3()
             }
+            
+            VStack {
+                
+                Spacer()
+                HStack(alignment: .center, spacing: 8) {
+                    
+                    ForEach(0..<pageCount) { idx in
+                        Circle()
+                            .frame(width: circleSize, height: circleSize)
+                            .foregroundColor(Color.gray)
+                            .opacity(self.currentPage == idx ? 1 : 0.35)
+                            .onTapGesture() {
+                                self.currentPage = idx
+                                print("Page \(idx + 1) tapped!")
+                            }
+                    }
+                }
+                .padding(.bottom)
+            }
         }
     }
 }
-
-
