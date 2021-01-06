5c46a4e93d8d28785ccb97edc3030e33005ea062
Andras Pal <andr4s.pal@gmail.com>
2021-01-06 17:22:21 +0000

    Initial Commit

---
+++ CustomSwiftUIPager/CustomSwiftUIPagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUIPagerApp.swift
+//  CustomSwiftUIPager
+//
+//  Created by Andras Pal on 06/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUIPagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
